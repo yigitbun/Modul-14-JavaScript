066edecc85d5473fec628a6a16073033bbc9dbd9
Bunyamin Yigit <yigitbun@gmail.com>
2021-04-28 16:44:54 +0200

    update

--- assets/js/03-manipulate-dom.js
+++ assets/js/03-manipulate-dom.js
@@ -2,40 +2,49 @@
 // #headline = id="headline"
 // .headline = class="headline"
 
-console.log('-------------------');
-// var text = document.getElementById("headline").innerHTML;
-// console.log(text)
+// let headline = document.getElementById("headline").innerHTML;
+let headline = document.getElementById("headline");
+// console.log(headline.textContent)
 
 // change the text from the headline to:
 // „Cool, sie ist offen! :)“
-console.log('-------------------');
 // document.getElementById("headline").innerHTML = "Cool, sie ist offen! :)";
+headline.innerText = "Cool, sie ist offen! :)"
 
 console.log('-------------------');
 // set value of the input element #input to:
 // "Hier steht Text"
+let inputElement = document.getElementById("input");
+inputElement.value = "Hier steht Text";
 
 // var b = document.getElementById("input");
 // b.value = "Hier steht Text";
 // set the value of the input element #input
 // as the innerHtml of the div #output
 
+let outputElement = document.getElementById("output");
+outputElement.innerHTML = inputElement.value;
+
 // document.getElementById("output").innerHTML = b.value;
 // add the class bg-black to the body
 // if there it doesn't got it already
-
+if (!document.body.classList.contains("bg-black")) {
+  document.body.classList.add("bg-black")
+}
 
 // toggle the class border-red on the input
+inputElement.classList.toggle("border-red");
 
 // multiply the data-number attribute by 3
 // and update the value in the dom
+outputElement.dataset.number *=3
+console.log(outputElement.dataset.number);
 
+// const input = document.querySelector('input');
+// const log = document.getElementById('output');
 
-const input = document.querySelector('input');
-const log = document.getElementById('output');
+// input.addEventListener('input', updateValue);
 
-input.addEventListener('input', updateValue);
-
-function updateValue(e) {
-  log.textContent = e.target.value;
-}
+// function updateValue(e) {
+//   log.textContent = e.target.value;
+// }
