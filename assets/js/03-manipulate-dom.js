6dff1dfb310365b43908ec1ba9c7537d655e78aa
Bunyamin Yigit <yigitbun@gmail.com>
2021-04-28 15:38:50 +0200

    update

--- assets/js/03-manipulate-dom.js
+++ assets/js/03-manipulate-dom.js
@@ -2,18 +2,25 @@
 // #headline = id="headline"
 // .headline = class="headline"
 
-var text = document.getElementById("headline").innerHTML;
-console.log(text)
+console.log('-------------------');
+// var text = document.getElementById("headline").innerHTML;
+// console.log(text)
 
 // change the text from the headline to:
 // „Cool, sie ist offen! :)“
-document.getElementById("headlibne").innerHTML = "Cool, sie ist offen! :)";
-// set value of the input element #input to:
-// „Hier steht Text“
+console.log('-------------------');
+// document.getElementById("headline").innerHTML = "Cool, sie ist offen! :)";
 
+console.log('-------------------');
+// set value of the input element #input to:
+// "Hier steht Text"
+
+// var b = document.getElementById("input");
+// b.value = "Hier steht Text";
 // set the value of the input element #input
 // as the innerHtml of the div #output
 
+// document.getElementById("output").innerHTML = b.value;
 // add the class bg-black to the body
 // if there it doesn't got it already
 
@@ -22,3 +29,13 @@ document.getElementById("headlibne").innerHTML = "Cool, sie ist offen! :)";
 
 // multiply the data-number attribute by 3
 // and update the value in the dom
+
+
+const input = document.querySelector('input');
+const log = document.getElementById('output');
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+  log.textContent = e.target.value;
+}
